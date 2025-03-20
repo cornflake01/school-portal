b0bda2f7c8cff8d3b2de4c81ee7af7ce04e27ed1
cornflake01 <frncsclp@gmail.com>
2025-03-20 20:40:05 +0800

    Fixing the Ui and working routes.

--- resources/views/admin/announcements.blade.php
+++ resources/views/admin/announcements.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
     <h1 class="text-3xl font-bold">School Announcements</h1>
-    <p class="text-gray-600 mt-2">Post and update school announcements.</p>
+    <p class="text-gray-600 mt-2">In this page, the school announcements will be posted..</p>
 @endsection
